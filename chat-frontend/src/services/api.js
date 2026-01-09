72e86f35904163aacc8cacd864cf50b8d355ca9c
Prakhar Pandey <prakhar@thetarzanway.com>
2026-01-10 02:00:42 +0530

    Fix:: Timings update

--- chat-frontend/src/services/api.js
+++ chat-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5500/api/v1' || 'https://chatapplication-1-gspo.onrender.com/api/v1',
+  baseURL:  'https://chatapplication-1-gspo.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
